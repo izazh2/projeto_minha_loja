3015a89ad060c3c938285f9c12aba7a843c14870
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-07-06 19:38:19 -0300

    Diversas alterações no js/produtos.js

---
+++ js/produtos.js
@@ -0,0 +1,164 @@
+const produtos = [
+    {
+        id_produto: 1,
+        descricao_produto: "Roupa de Santo Branca Feminina",
+        caminho_da_imagem: "../img/roupa_branca_feminina.jpg",
+        valor_unitario: 320.00,
+        id_secao: 1,
+        nome_secao: "Roupas Femininas"
+    },
+    {
+        id_produto: 2,
+        descricao_produto: "Roupa de Santo Branca Masculina",
+        caminho_da_imagem: "../img/roupa_branca_masculina.jpg",
+        valor_unitario: 310.00,
+        id_secao: 2,
+        nome_secao: "Roupas Masculinas"
+    },
+    {
+        id_produto: 3,
+        descricao_produto: "Saia Rodada em Renda",
+        caminho_da_imagem: "../img/saia_renda.jpg",
+        valor_unitario: 180.00,
+        id_secao: 1,
+        nome_secao: "Roupas Femininas"
+    },
+    {
+        id_produto: 4,
+        descricao_produto: "Calça Branca de Algodão",
+        caminho_da_imagem: "../img/calca_branca.jpg",
+        valor_unitario: 120.00,
+        id_secao: 2,
+        nome_secao: "Roupas Masculinas"
+    },
+    {
+        id_produto: 5,
+        descricao_produto: "Fio de Contas Verde",
+        caminho_da_imagem: "../img/fio_contas_verde.jpg",
+        valor_unitario: 90.00,
+        id_secao: 3,
+        nome_secao: "Guias e Fios"
+    },
+    {
+        id_produto: 6,
+        descricao_produto: "Fio de Contas Branco",
+        caminho_da_imagem: "../img/fio_contas_branco.jpg",
+        valor_unitario: 95.00,
+        id_secao: 3,
+        nome_secao: "Guias e Fios"
+    },
+    {
+        id_produto: 7,
+        descricao_produto: "Bata Branca Bordada",
+        caminho_da_imagem: "../img/bata_bordada.jpg",
+        valor_unitario: 210.00,
+        id_secao: 2,
+        nome_secao: "Roupas Masculinas"
+    },
+    {
+        id_produto: 8,
+        descricao_produto: "Torço Branco",
+        caminho_da_imagem: "../img/torco_branco.jpg",
+        valor_unitario: 65.00,
+        id_secao: 4,
+        nome_secao: "Acessórios"
+    },
+    {
+        id_produto: 9,
+        descricao_produto: "Ojá Branco",
+        caminho_da_imagem: "../img/oja_branco.jpg",
+        valor_unitario: 55.00,
+        id_secao: 4,
+        nome_secao: "Acessórios"
+    },
+    {
+        id_produto: 10,
+        descricao_produto: "Conjunto Feminino em Renda",
+        caminho_da_imagem: "../img/conjunto_renda.jpg",
+        valor_unitario: 450.00,
+        id_secao: 1,
+        nome_secao: "Roupas Femininas"
+    },
+    {
+        id_produto: 11,
+        descricao_produto: "Vestido Branco Tradicional",
+        caminho_da_imagem: "../img/vestido_branco.jpg",
+        valor_unitario: 380.00,
+        id_secao: 1,
+        nome_secao: "Roupas Femininas"
+    },
+    {
+        id_produto: 12,
+        descricao_produto: "Pulseira de Contas",
+        caminho_da_imagem: "../img/pulseira_contas.jpg",
+        valor_unitario: 35.00,
+        id_secao: 4,
+        nome_secao: "Acessórios"
+    },
+    {
+        id_produto: 13,
+        descricao_produto: "Imagem Decorativa de Oxóssi",
+        caminho_da_imagem: "../img/imagem_oxossi.jpg",
+        valor_unitario: 150.00,
+        id_secao: 5,
+        nome_secao: "Artigos Religiosos"
+    },
+    {
+        id_produto: 14,
+        descricao_produto: "Quartinha Branca",
+        caminho_da_imagem: "../img/quartinha.jpg",
+        valor_unitario: 85.00,
+        id_secao: 5,
+        nome_secao: "Artigos Religiosos"
+    },
+    {
+        id_produto: 15,
+        descricao_produto: "Alguidar de Barro",
+        caminho_da_imagem: "../img/alguidar.jpg",
+        valor_unitario: 70.00,
+        id_secao: 5,
+        nome_secao: "Artigos Religiosos"
+    },
+    {
+        id_produto: 16,
+        descricao_produto: "Camisa Branca Tradicional",
+        caminho_da_imagem: "../img/camisa_branca.jpg",
+        valor_unitario: 140.00,
+        id_secao: 2,
+        nome_secao: "Roupas Masculinas"
+    },
+    {
+        id_produto: 17,
+        descricao_produto: "Saia de Algodão Branca",
+        caminho_da_imagem: "../img/saia_algodao.jpg",
+        valor_unitario: 160.00,
+        id_secao: 1,
+        nome_secao: "Roupas Femininas"
+    },
+    {
+        id_produto: 18,
+        descricao_produto: "Guia Verde e Branca",
+        caminho_da_imagem: "../img/guia_verde_branca.jpg",
+        valor_unitario: 110.00,
+        id_secao: 3,
+        nome_secao: "Guias e Fios"
+    },
+    {
+        id_produto: 19,
+        descricao_produto: "Brajá Tradicional",
+        caminho_da_imagem: "../img/braja.jpg",
+        valor_unitario: 180.00,
+        id_secao: 3,
+        nome_secao: "Guias e Fios"
+    },
+    {
+        id_produto: 20,
+        descricao_produto: "Leque Branco Rendado",
+        caminho_da_imagem: "../img/leque_branco.jpg",
+        valor_unitario: 75.00,
+        id_secao: 4,
+        nome_secao: "Acessórios"
+    }
+];
+
+export{produtos}
