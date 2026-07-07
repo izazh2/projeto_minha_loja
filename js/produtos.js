7f34a35d13335023f53e5e448198a88102956eeb
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-07-07 19:10:42 -0300

    Diversas alterações no arquivo js/produtos.js

--- js/produtos.js
+++ js/produtos.js
@@ -1,128 +1,128 @@
 const produtos = [
     {
         id_produto: 1,
-        descricao_produto: "Roupa de Santo Branca Feminina",
-        caminho_da_imagem: "../img/roupa_branca_feminina.jpg",
-        valor_unitario: 320.00,
+        descricao_produto: "Roupa de Maria Navalha",
+        caminho_da_imagem: "../img/imagem01.jpg",
+        valor_unitario: 1500.00,
         id_secao: 1,
-        nome_secao: "Roupas Femininas"
+        nome_secao: "Roupas Catiços"
     },
     {
         id_produto: 2,
-        descricao_produto: "Roupa de Santo Branca Masculina",
-        caminho_da_imagem: "../img/roupa_branca_masculina.jpg",
-        valor_unitario: 310.00,
+        descricao_produto: "Roupa de Iemanja",
+        caminho_da_imagem: "../img/imagem02.jpg",
+        valor_unitario: 1900.00,
         id_secao: 2,
-        nome_secao: "Roupas Masculinas"
+        nome_secao: "Roupas de Santo"
     },
     {
         id_produto: 3,
-        descricao_produto: "Saia Rodada em Renda",
-        caminho_da_imagem: "../img/saia_renda.jpg",
-        valor_unitario: 180.00,
-        id_secao: 1,
+        descricao_produto: "Baina",
+        caminho_da_imagem: "../img/imagem03.jpg",
+        valor_unitario: 2000.00,
+        id_secao: 3,
         nome_secao: "Roupas Femininas"
     },
     {
         id_produto: 4,
-        descricao_produto: "Calça Branca de Algodão",
-        caminho_da_imagem: "../img/calca_branca.jpg",
-        valor_unitario: 120.00,
+        descricao_produto: "Roupa Oxala",
+        caminho_da_imagem: "../img/imagem04.jpg",
+        valor_unitario: 1200.00,
         id_secao: 2,
-        nome_secao: "Roupas Masculinas"
+        nome_secao: "Roupas de Santo"
     },
     {
         id_produto: 5,
-        descricao_produto: "Fio de Contas Verde",
-        caminho_da_imagem: "../img/fio_contas_verde.jpg",
-        valor_unitario: 90.00,
-        id_secao: 3,
-        nome_secao: "Guias e Fios"
+        descricao_produto: "Roupa de Oxum",
+        caminho_da_imagem: "../img/imagem05.jpg",
+        valor_unitario: 2500.00,
+        id_secao: 2,
+        nome_secao: "Roupas de Santo"
     },
     {
         id_produto: 6,
-        descricao_produto: "Fio de Contas Branco",
-        caminho_da_imagem: "../img/fio_contas_branco.jpg",
-        valor_unitario: 95.00,
-        id_secao: 3,
-        nome_secao: "Guias e Fios"
+        descricao_produto: "Roupa de Xangô",
+        caminho_da_imagem: "../img/imagem06.jpg",
+        valor_unitario: 2500.00,
+        id_secao: 2,
+        nome_secao: "Roupas de Santo"
     },
     {
         id_produto: 7,
-        descricao_produto: "Bata Branca Bordada",
-        caminho_da_imagem: "../img/bata_bordada.jpg",
-        valor_unitario: 210.00,
+        descricao_produto: "Roupa de Odé",
+        caminho_da_imagem: "../img/imagem07.jpg",
+        valor_unitario: 2100.00,
         id_secao: 2,
-        nome_secao: "Roupas Masculinas"
+        nome_secao: "Roupas de Santo"
     },
     {
         id_produto: 8,
-        descricao_produto: "Torço Branco",
-        caminho_da_imagem: "../img/torco_branco.jpg",
-        valor_unitario: 65.00,
-        id_secao: 4,
-        nome_secao: "Acessórios"
+        descricao_produto: "Roupa de iansã",
+        caminho_da_imagem: "../img/imagem08.jpg",
+        valor_unitario: 2000.00,
+        id_secao: 2,
+        nome_secao: "Roupas de Santo"
     },
     {
         id_produto: 9,
-        descricao_produto: "Ojá Branco",
-        caminho_da_imagem: "../img/oja_branco.jpg",
-        valor_unitario: 55.00,
-        id_secao: 4,
-        nome_secao: "Acessórios"
+        descricao_produto: "Roupa de Ogum",
+        caminho_da_imagem: "../img/imagem09.jpg",
+        valor_unitario: 1500.00,
+        id_secao: 2,
+        nome_secao: "Roupas de Santo"
     },
     {
         id_produto: 10,
-        descricao_produto: "Conjunto Feminino em Renda",
-        caminho_da_imagem: "../img/conjunto_renda.jpg",
-        valor_unitario: 450.00,
-        id_secao: 1,
-        nome_secao: "Roupas Femininas"
+        descricao_produto: "Roupa de iemanja",
+        caminho_da_imagem: "../img/imagem10.jpg",
+        valor_unitario: 4500.00,
+        id_secao: 2,
+        nome_secao: "Roupas de Santo"
     },
     {
         id_produto: 11,
-        descricao_produto: "Vestido Branco Tradicional",
-        caminho_da_imagem: "../img/vestido_branco.jpg",
-        valor_unitario: 380.00,
-        id_secao: 1,
-        nome_secao: "Roupas Femininas"
+        descricao_produto: "Roupa de Oba",
+        caminho_da_imagem: "../img/imagem11.jpg",
+        valor_unitario: 3800.00,
+        id_secao: 2,
+        nome_secao: "Roupas de Santo"
     },
     {
         id_produto: 12,
-        descricao_produto: "Pulseira de Contas",
-        caminho_da_imagem: "../img/pulseira_contas.jpg",
-        valor_unitario: 35.00,
-        id_secao: 4,
-        nome_secao: "Acessórios"
+        descricao_produto: "Roupa de Oxala 2",
+        caminho_da_imagem: "../img/imagem12.jpg",
+        valor_unitario: 3500.00,
+        id_secao: 2,
+        nome_secao: "Roupas de Santo"
     },
     {
         id_produto: 13,
-        descricao_produto: "Imagem Decorativa de Oxóssi",
-        caminho_da_imagem: "../img/imagem_oxossi.jpg",
-        valor_unitario: 150.00,
-        id_secao: 5,
-        nome_secao: "Artigos Religiosos"
+        descricao_produto: "Roupa de Xangô",
+        caminho_da_imagem: "../img/imagem13.jpg",
+        valor_unitario: 1500.00,
+        id_secao: 2,
+        nome_secao: "Roupas de Santo"
     },
     {
         id_produto: 14,
-        descricao_produto: "Quartinha Branca",
-        caminho_da_imagem: "../img/quartinha.jpg",
-        valor_unitario: 85.00,
-        id_secao: 5,
-        nome_secao: "Artigos Religiosos"
+        descricao_produto: "Roupa Onira",
+        caminho_da_imagem: "../img/imagem14.jpg",
+        valor_unitario: 2000.00,
+        id_secao: 2,
+        nome_secao: "Roupas de Santo"
     },
     {
         id_produto: 15,
-        descricao_produto: "Alguidar de Barro",
-        caminho_da_imagem: "../img/alguidar.jpg",
-        valor_unitario: 70.00,
-        id_secao: 5,
-        nome_secao: "Artigos Religiosos"
+        descricao_produto: "Roupa de Oxala 3",
+        caminho_da_imagem: "../img/imagem15.jpg",
+        valor_unitario: 1800.00,
+        id_secao: 2,
+        nome_secao: "Roupas de Santo"
     },
     {
         id_produto: 16,
         descricao_produto: "Camisa Branca Tradicional",
-        caminho_da_imagem: "../img/camisa_branca.jpg",
+        caminho_da_imagem: "../img/imagem.jpg",
         valor_unitario: 140.00,
         id_secao: 2,
         nome_secao: "Roupas Masculinas"
