85cb924e525363dc32d8de57a5570d98066ca16d
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-07-09 19:14:46 -0300

    Diversas alterações no arquivo js/carrega_produtos.js

--- js/carrega_produtos.js
+++ js/carrega_produtos.js
@@ -34,7 +34,62 @@ const listaProdutos = () =>{
         divCard.appendChild(btnCard)
 
         selection_cards.appendChild(divCard)
+
     })
 }
 
 listarProdutos()
+
+//FILTRANDO AS SEÇÕES COM A COLEÇÃO  map
+const listarSecoes = () => {
+    //CRIANDO A COLEÇÃO MAP
+    const secoesFiltrada = new Map()
+
+    //PECORRENDO O ARRAY PRODUTOS E FILTANDO AS SEÇÕES
+    produtos.forEach((elem, i) =>{
+        //CRIANDO A CHAVE E O VALOR DA COLEÇÃO MAP A PARTIR DO ID DA SEÇÃO DA LISTA DE PRODUTOS
+        secoesFiltrada.set(elem.id_secao, elem)
+    })
+
+    //CONVERTENDO O MAP EM ARRAY
+    const secosMenu = Array.from(secoesFiltrada.values())
+
+    //RETORNANDO O ARRAY CONVERTIDO
+    return secosMenu
+
+}
+
+//MONTANDO OS LINKS SEÇÕES
+const montarSecoes = () =>{
+    //PEGANGO O ELEMENTO DO DOM 
+    const ulMenu = document.querySelector('#menu-secoes')
+    //LIMPANDO O ELEMENTO ulMenu
+    ulMenu.innerHTML = ''
+
+    //PECORRENDO O ARRAY DAS SEÇÕES FILTRADA
+    listarSecoes().forEach((elem,i) =>{
+        //CRIANDO O ELEMENTO li
+        const liSecoes = document.createAttribute('li')
+
+        //CRIANDO O ELEMENTO a
+        const aSecao = document.childElement('a')
+        aSecao.setAttribute('href', '#')
+        aSecao.setAttribute('class', 'lnk-secao')
+        aSecao.innerHTML = elem.secao 
+
+        //CAPTURANDO O CLICK DOS LINKS
+        aSecao.addEventListener('click', () =>{
+            //PARA TESTE
+                console.log(elem.id_secao)
+        })
+
+        //ADCIONANDO O ELEMENTO FILHO a NO ELEMENTO li
+        liSecao.appendChild(aSecao)
+
+        //ADICIONANDO O ELEMENTO FILHO li NO ELEMENTO DO DOM ul
+        ulMenu.appendChild(aSecao)
+    })
+
+}
+
+montarSecoes()
