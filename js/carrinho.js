ff7d0265755dca88600613f6fdaa3a9a1d153f5d
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-07-14 19:28:14 -0300

    Diversas alterações no arquivo js/carrinho.js

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,38 +1,29 @@
-//CRIANDO O ARRAYA DE ITENS DO CARRINHO
-const itensCarrinho = JSON.parse(localStorage.getItem('itensSessao'))|| []
+//localStorage.removeItem("itensSessao");
+//localStorage.clear()
+
+//CRIANDO O ARRAY DE ITENS DO CARRINHO
+const itensCarrinho = JSON.parse(localStorage.getItem('itensSessao')) || []
+
+//const itensCarrinho = JSON.parse(sessionStorage.getItem('itensSessao')) || []
+
 
 //FUNÇÃO PARA ADCIONAR O ITEM NO ARRAY
 const addItem = (objItem) => {
     itensCarrinho.push(objItem)
 
-    localStorage.setItem('itensSessao', itensCarrinho)
+    localStorage.setItem('itensSessao', JSON.stringify(itensCarrinho))
+    //sessionStorage.setItem('itensSessao', JSON.stringify(itensCarrinho))
 }
 
 //LISTAR ITENS DO CARRINHO
 const listItens = () => {
-    const itensSelecionados = JSON.stringify(localStorage.getItem('itensSessao'))
 
-    return itensCarrinho
+    const itensSelecionados = JSON.parse(localStorage.getItem('itensSessao')) || []
+
+    //const itensSelecionados = JSON.parse(sessionStorage.getItem('itensSessao')) || []
+
+    return itensSelecionados
 }
 
-//MONTANDO A TELA CARRINHO
-const montaTelaCarrinho = () =>{
-    //PEGANDO ELEMENTOS DO DOM
-    const sectionItensCarrinho = document.querySelector('#itens-carrinho')
 
-    listItens().forEach(elem => {
-        const sectionItem = document.createElement('section')
-        sectionItem.setAttribute('class', 'item')
-        sectionItem.innerHTML = `<img src = '${elem.carrinhp_da_imagem}' alt+${eleme.descricao_produto}/> 
-        <p class='descricao'>${elem.descricao_produto} </p> 
-        <p class='vlr-unitario>${elem.valor_unitario}</p>
-        <input type="number" name='quant${i}' id='quant${i}' class="input-item" value=${1}> 
-        <p class="tot-item">${elem.valor_unitatio * 1} </p>
-        <img scr+"../imagens/icones/remover.png" alt="" class="img-remover">`
-
-        sectionItensCarrinho.appendChild(sectionItem)
-        });
-
-}
-
-export{addItem}
+export { addItem, listItens }
