2a76ee442a54d1ea6f8fc12909577557d637aab6
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-07-14 21:31:48 -0300

    Diversas alterações no aquivo js/monta_tela_carrinho.js

--- js/monta_tela_carrinho.js
+++ js/monta_tela_carrinho.js
@@ -3,39 +3,38 @@ import { listItens, removeriItem } from "./carrinho.js";
 const montaTelaCarrinho = () =>{
     //PEGANDO ELEMENTOS DO DOM
     const sectionItensCarrinho = document.querySelector('#lista-itens')
+    sectionItensCarrinho.innerHTML = ''
 
-    listItens().forEach(elem => {
+    listItens().forEach((elem, i) => {
         const sectionItem = document.createElement('section')
         sectionItem.setAttribute('class', 'item')
-        sectionItem.innerHTML = `<img src = '${elem.carrinhp_da_imagem}' alt+${eleme.descricao_produto}/> 
-        <p class='descricao'>${elem.descricao_produto} </p> 
-        <p class='vlr-unitario>${elem.valor_unitario}</p>
-        <input type="number" name='quant${i}' id='quant${i}' class="input-item" value=${1}> 
-        <p class="tot-item">${elem.valor_unitatio * 1} </p>`
+        sectionItem.innerHTML = `<img src='${elem.caminho_da_imagem}' alt='${elem.descricao_produto}'/>
+        <p class='descricao'>${elem.descricao_produto} </p>
+        <p class='vlr-unitario'>${elem.valor_unitario}</p>
+        <input type="number" name='quant${i}' id='quant${i}' class="input-item" value=${1}>
+        <p class="tot-item">${elem.valor_unitario * 1} </p>`
 
         const imgRemover = document.createElement('img')
         imgRemover.setAttribute('src', '../imagem/icones/remover.png')
         imgRemover.setAttribute('alt', 'Remover')
         imgRemover.setAttribute('class','img-remover')
 
-        imgRemover.addEventListener('click'), () => {
+        imgRemover.addEventListener('click', () => {
             if(confirm(`Deseja remover ${elem.descricao_produto} da sua lista?`)){
                 removerItemCarrinho(i)
             }
-        } 
+        })
 
-        sectionItem.addEventListener(imgRemover)
+        sectionItem.appendChild(imgRemover)
 
         sectionItensCarrinho.appendChild(sectionItem)
-        });
-    }
+    });
+}
 
 const removerItemCarrinho = (pos) => {
     removeriItem(pos);
 
     montaTelaCarrinho();
 }
-
-
 
 montaTelaCarrinho()
